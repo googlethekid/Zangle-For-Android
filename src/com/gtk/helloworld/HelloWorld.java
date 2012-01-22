fd17b27f450b782353b2f49b29a69d3d74cc4f78
Kyle Huttger <khuttger@gmail.com>
2012-01-21 23:29:53 -0800

    First commit

---
+++ src/com/gtk/helloworld/HelloWorld.java
@@ -0,0 +1,19 @@
+package com.gtk.helloworld;
+
+import android.app.Activity;
+import android.os.Bundle;
+import java.net.*;
+import java.io.*;
+
+
+public class HelloWorld extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
+
+
+	      
