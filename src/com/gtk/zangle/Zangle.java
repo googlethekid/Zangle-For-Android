8730190bbf3645b697a441e30d4ed33baac95c55
Kyle Huttger <khuttger@gmail.com>
2012-01-21 23:40:40 -0800

    First Commit

--- src/com/gtk/zangle/Zangle.java
+++ src/com/gtk/zangle/Zangle.java
@@ -1,12 +1,14 @@
-package com.gtk.helloworld;
+package com.gtk.zangle;
 
 import android.app.Activity;
 import android.os.Bundle;
 import java.net.*;
 import java.io.*;
 
+import com.gtk.helloworld.R;
 
-public class HelloWorld extends Activity {
+
+public class Zangle extends Activity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
